5fe043f623f2c67cb306d38cc7b73662dd89bd1e
Aleks <jokerka17@gmail.com>
2019-04-24 18:04:33 -0700

    Initial setup

---
+++ Software.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Software{
+    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to webuildsoftware.com!");
+    }
+
+    public static void menu()
+    {
+        System.out.println("Please choose an operation:");
+        System.out.println("1. Create a Sprint for a project\n2. Create a project\n3. Add user stories to sprint backlog" + 
+        "\n4 Display developer(s) and/or Sprint(s)\n5. List developers that are part of a Sprint");
+        //Create Read Update Delete
+
+    }
+
+}
